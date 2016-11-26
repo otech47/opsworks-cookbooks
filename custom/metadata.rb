c6ea429f87a3270e340c48cf20b38afab77f53d7
Oscar Lafarga <otech47@gmail.com>
2016-11-26 12:54:12 -0500

    git depends

---
+++ custom/metadata.rb
@@ -0,0 +1,17 @@
+name 'custom'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures custom'
+long_description 'Installs/Configures custom'
+version '0.1.0'
+
+depends 'git', '~> 4.3'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/custom/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/custom' if respond_to?(:source_url)
