aeefad733a655f1e073ef5b2972533499bb81834
Oscar Lafarga <otech47@gmail.com>
2017-02-08 10:12:53 -0500

    test

--- ssh_host_keys/recipes/default.rb
+++ ssh_host_keys/recipes/default.rb
@@ -1,12 +1,14 @@
+
+instance = search('aws_opsworks_instance').first
 p "ssh_host_rsa_key_private"
-p node['ssh_host_rsa_key_private']
+p instance['ssh_host_rsa_key_private']
 p "ssh_host_rsa_key_public"
-p node['ssh_host_rsa_key_public']
+p instance['ssh_host_rsa_key_public']
 p "ssh_host_dsa_key_private"
-p node['ssh_host_dsa_key_private']
+p instance['ssh_host_dsa_key_private']
 p "ssh_host_dsa_key_public"
-p node['ssh_host_dsa_key_public']
-write_host_key(:path => '/etc/ssh/ssh_host_rsa_key', :content => node['ssh_host_rsa_key_private'], :mode => 0600)
-write_host_key(:path => '/etc/ssh/ssh_host_rsa_key.pub', :content => node['ssh_host_rsa_key_public'], :mode => 0644)
-write_host_key(:path => '/etc/ssh/ssh_host_dsa_key', :content => node['ssh_host_dsa_key_private'], :mode => 0600)
-write_host_key(:path => '/etc/ssh/ssh_host_dsa_key.pub', :content => node['ssh_host_dsa_key_public'], :mode => 0644)
+p instance['ssh_host_dsa_key_public']
+write_host_key(:path => '/etc/ssh/ssh_host_rsa_key', :content => instance['ssh_host_rsa_key_private'], :mode => 0600)
+write_host_key(:path => '/etc/ssh/ssh_host_rsa_key.pub', :content => instance['ssh_host_rsa_key_public'], :mode => 0644)
+write_host_key(:path => '/etc/ssh/ssh_host_dsa_key', :content => instance['ssh_host_dsa_key_private'], :mode => 0600)
+write_host_key(:path => '/etc/ssh/ssh_host_dsa_key.pub', :content => instance['ssh_host_dsa_key_public'], :mode => 0644)
